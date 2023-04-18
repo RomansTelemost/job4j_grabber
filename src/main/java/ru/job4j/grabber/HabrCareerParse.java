74cab9c1fbc86283fa0fa6fec9d0f3f8ede9a818
Roman Koshelev <termopsis@mail.ru>
2023-04-18 23:13:24 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,11 +5,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HabrCareerParse {
 
@@ -17,8 +22,12 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
+    private static final Map<String, String> LINK_DESCRIPTION_MAP = new HashMap<>();
 
+    private static final Logger LOG = LoggerFactory.getLogger(HabrCareerParse.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        List<String> links = new ArrayList<>();
         List<Integer> pageNumbers = List.of(1, 2, 3, 4, 5);
         for (Integer pageNumber : pageNumbers) {
             String numberedPageLink = PAGE_LINK + "?page=" + pageNumber;
@@ -39,7 +48,27 @@ public class HabrCareerParse {
                 String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), localDateTime.toLocalDate());
                 System.out.printf("%s %s%n", vacancyName, link);
                 System.out.println("-*-");
+
+                links.add(String.format("%s%s", SOURCE_LINK, linkElement.attr("href")));
             });
         }
+        links.stream().forEach(link -> LINK_DESCRIPTION_MAP.put(link, retrieveDescription(link)));
+        System.out.println(LINK_DESCRIPTION_MAP.get("https://career.habr.com/vacancies/1000122487"));
+    }
+
+    public static String retrieveDescription(String link) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            Connection connection = Jsoup.connect(link);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-description__text");
+            rows.forEach(row -> {
+                stringBuilder.append(row.text());
+                stringBuilder.append(System.lineSeparator());
+            });
+        } catch (IOException e) {
+            LOG.error("Pars description", e);
+        }
+        return stringBuilder.toString();
     }
 }
